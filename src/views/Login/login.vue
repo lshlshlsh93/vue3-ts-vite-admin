67869c2add18c038469e4cf68867ee0c12787045
lsh <596060075@qq.com>
2022-08-15 18:47:10 +0800

    update: import unplugin-vue-components and unplugin-auto-import

---
+++ src/views/Login/login.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>{{ message }}</div>
+  <span>{{ count }}</span> <br />
+  <el-button @click="count++">点我</el-button>
+</template>
+<script setup lang="ts">
+const message = ref<string>('我是登录页面')
+const count = ref(0)
+</script>
+<style lang="scss" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
+
+const pathSrc = path.resolve(__dirname, './src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  server: {
+    host: '0.0.0.0',
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@': pathSrc,
+    },
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      imports: ['vue'],
+      // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+      resolvers: [ElementPlusResolver()],
+      dts: path.resolve(pathSrc, 'auto-imports.d.ts'),
+    }),
+    Components({
+      resolvers: [
+        // 自动导入 Element Plus 组件
+        ElementPlusResolver(),
+      ],
+      dts: path.resolve(pathSrc, 'components.d.ts'),
+    }),
+  ],
 })
